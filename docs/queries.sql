aa7c7b39a051d548aabac2b8413f61bc7f935d65
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-06-13 13:44:38 +0100

    add buisness table

--- docs/queries.sql
+++ docs/queries.sql
@@ -4,3 +4,13 @@ USE biaawcheria;
 CREATE TABLE categories( `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
                           `category` VARCHAR(255) NOT NULL,
                           `photo` VARCHAR(255) );
+
+
+USE biaawcheria;
+CREATE TABLE business(id int(15) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                      id_category int(15),
+                      name varchar(255) NOT NULL,
+                      photo varchar(255),
+                      description text NOT NULL, 
+                      FOREIGN KEY(id_category) REFERENCES categories(id)
+                     );
